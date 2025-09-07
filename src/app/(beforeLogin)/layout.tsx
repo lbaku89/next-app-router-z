e6ac607d758bc0d55bb1f86019c61d3fe0cf7d22
hyunwoo <khj930410@naver.com>
2025-09-07 09:27:16 +0900

    feat: parallel routing

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,8 +1,14 @@
 import { ReactNode } from "react";
+import styles from "@/app/(beforeLogin)/_component/main.module.css";
 
 type Props = { children: ReactNode; modal: ReactNode };
-export default function Layout({ children }: Props) {
-  return <div>{children}</div>;
+export default function Layout({ children, modal }: Props) {
+  return (
+    <div className={styles.container}>
+      {children}
+      {modal}
+    </div>
+  );
 }
 
 // 주소가 localhost:3001일 때는 children->page.tsx, modal->@modal/default.tsx
